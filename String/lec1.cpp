4fd882d9184962020b8bfa7b752acf97d5c95c08
SakshamManav <manavsaksham06@gmail.com>
2025-08-21 21:50:19 +0530

    Solved string part 2 - 1 to 3

--- String/lec1.cpp
+++ String/lec1.cpp
@@ -142,59 +142,231 @@ using namespace std;
 //     sol.reverseWords(s);
 // }
 
-
 // Longest Common Prefix (done by myself without any help)
 
-
 // our answer string size will always from zero to smallest string in the vector.
-class Solution
-{
-public:
-    string longestCommonPrefix(vector<string> &strs)
-    {
-        int max_idx = INT_MAX;
-        for (int i = 0; i < strs.size(); i++)
-        {
-            max_idx = min(max_idx, (int)strs[i].size());
-        }
-        if(max_idx == 0){
-            return "";
-        }
+// class Solution
+// {
+// public:
+//     string longestCommonPrefix(vector<string> &strs)
+//     {
+//         int max_idx = INT_MAX;
+//         for (int i = 0; i < strs.size(); i++)
+//         {
+//             max_idx = min(max_idx, (int)strs[i].size());
+//         }
+//         if(max_idx == 0){
+//             return "";
+//         }
+
+//         string ans = strs[0].substr(0, max_idx);
+
+//         int cur_idx = 0;
+//         int j = 0;
+//         int check = max_idx;
+
+//         while (j < max_idx)
+//         {
+//             bool is_true = true;
+//             for (int i = 0; i < strs.size(); i++)
+//             {
+//                 if(i == strs.size()-1 && strs[i].substr(0, ans.size()) == ans) {
+//                     return ans;
+//                 }
+//                 if (strs[i].substr(0, ans.size()) != ans){
+//                     ans.resize(ans.size()-1);
+//                     is_true = false;
+//                     break;
+//                 }
+//             }
+//             // cout<<ans<<" ";
+//             if(is_true){
+//                 return ans;
+//             }
+//         j++;
+//         }
+//         return "";
+//     }
+// };
+
+// int main()
+// {
+//     vector<string> str = {"flower","flow","flight"};
+//     Solution sol;
+//     cout<<sol.longestCommonPrefix(str);
+// }
+
+// rotate string
+
+// brute force - tle
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         int n = s.size();
+//         if(s == goal){
+//             return true;
+//         }
+//         // int check_ans = "";
+//         for(int i=1; i<s.size(); i++){
+//             int d = i % s.size();
+//             string substring = s.substr(n - d) + s.substr(0, n - d);
+//             if(substring == goal){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// Optimal
+//  class Solution {
+//  public:
+//      bool rotateString(string s, string goal) {
+//         if (s.size() != goal.size()) return false;
+//          string doubled = s + s;
+//          return doubled.find(goal) != string::npos;
+//      }
+//  };
+
+// Valid anagram
+
+// taking extra space, we can also do it by using sorting
+// class Solution
+// {
+// public:
+//     bool isAnagram(string s, string t)
+//     {
+//         unordered_map<char, int> mp;
+//         if (s.size() != t.size())
+//         {
+//             return false;
+//         }
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             mp[s[i]]++;
+//         }
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             if(mp[t[i]] > 0){
+//                 mp[t[i]]--;
+//             }else{
+//                 return false;
+//             }
+//         }
+//         return true;
+
+//     }
+// };
+
+// int main()
+// {
+//     Solution sol;
+//     string s = "anagram", t = "nagarmm";
+//     cout << sol.isAnagram(s, t);
+// }
+// class Solution
+// {
+// public:
+//  static bool cmp(pair<char, int> a, pair<char, int> b)
+//         {
+//             return a.second > b.second;
+//         }
+//     string frequencySort(string s)
+//     {
+//         unordered_map<char, int> mp;
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             mp[s[i]]++;
+//         }
+//         vector<pair<char, int>> vc;
+//         for (auto &p : mp)
+//             vc.push_back(p);
+
+//         sort(vc.begin(), vc.end(), cmp);
+
+//         string result = "";
+
+//         for (auto &p : vc)
+//             result.append(p.second, p.first);
+
+//         return result;
+//     }
+// };
+
+
+
+// Roman to integer 
+
+// this solution will work but we are doing some hardcore checks
+// class Solution
+// {
+// public:
+//     int romanToInt(string s)
+//     {
+//         unordered_map<char, int> mp = {
+//             {'I', 1},
+//             {'V', 5},
+//             {'X', 10},
+//             {'L', 50},
+//             {'C', 100},
+//             {'D', 500},
+//             {'M', 1000}};
+//             int ans = 0;
+//         for (int i = 0; i < s.size()-1; i++)
+//         {
+//             if((s[i] == 'I' && s[i+1] == 'V') || (s[i] == 'I' && s[i+1] == 'X')){
+//                 ans += mp[s[i+1]] - mp[s[i]];
+//                 i++;
+//             }
+//             else if((s[i] == 'X' && s[i+1] == 'L') || (s[i] == 'X' && s[i+1] == 'C')){
+//                 ans += mp[s[i+1]] - mp[s[i]];
+//                 i++;
+//             }
+//             else if((s[i] == 'C' && s[i+1] == 'D') || (s[i] == 'C' && s[i+1] == 'M')){
+//                 ans += mp[s[i+1]] - mp[s[i]];
+//                 i++;
+//             }
+//             else{
+//                 ans += mp[s[i]];
+//             }
+//         }
         
-        
-        string ans = strs[0].substr(0, max_idx);
-        
-        int cur_idx = 0;
-        int j = 0;
-        int check = max_idx;
-        
-        while (j < max_idx)
-        {
-            bool is_true = true;
-            for (int i = 0; i < strs.size(); i++)
-            {
-                if(i == strs.size()-1 && strs[i].substr(0, ans.size()) == ans) {
-                    return ans;
-                }
-                if (strs[i].substr(0, ans.size()) != ans){
-                    ans.resize(ans.size()-1);
-                    is_true = false;
-                    break;
-                }
-            }
-            // cout<<ans<<" ";
-            if(is_true){
-                return ans;
-            }
-        j++;
-        }
-        return "";
-    }
-};
+//         return ans;
+//     }
+// };
+
+
+// in this we are not doing hardcore checks
+
+// class Solution {
+// public:
+//     int romanToInt(string &s) {
+//         unordered_map<char, int> mp = {
+//             {'I', 1},
+//             {'V', 5},
+//             {'X', 10},
+//             {'L', 50},
+//             {'C', 100},
+//             {'D', 500},
+//             {'M', 1000}
+//         };
+
+//         int ans = 0;
+//         for (int i = 0; i < s.size(); i++) {
+//             if (i + 1 < s.size() && mp[s[i]] < mp[s[i + 1]]) {
+//                 ans -= mp[s[i]];
+//             } else {
+//                 ans += mp[s[i]];
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
 int main()
 {
-    vector<string> str = {"flower","flow","flight"};
+    string str = "MCMXCIV";
+    
     Solution sol;
-    cout<<sol.longestCommonPrefix(str);
+    cout << sol.romanToInt(str);
 }
--- practice.js
+++ practice.js
@@ -1,13 +1,56 @@
-let a = "8"
-let b = 9
+// let a = "8"
+// let b = 9
+// var ab = "saksham"
+// // console.log(typeof(a+b))
 
-console.log(typeof(a+b))
+// const c = 9;
 
-const c = 9;
+// const obj ={
+//     word:"meaning",
+//     "Play":"khelna",
+//     "DAnce":"nachna"
+// }
+// console.log(obj)
+// console.log(2**5);
 
-const obj ={
-    "word":"meaning",
-    "Play":"khelna",
-    "DAnce":"nachna"
+// function hello(){
+//     let a = "saksham";
+//     ab = 98;
+//     c = 23;
+//     console.log(c)
+//     // console.log(a);
+// }
+//     // console.log(ab)
+// console.log(c)
+
+// hello();
+// // console.log(ab)
+// // console.log(a)
+
+// var a;
+// let b;
+// console.log(a);
+// console.log(b);
+
+function sum( a,  b){
+    return a+b;
 }
-console.log(obj)
+function greet(name) {
+  return "Hello, " + name; // returns a String
+}
+
+const obj = {
+    a:20,
+    b:45,
+    c:32,
+}
+
+// for(let i=0; i<obj)
+
+for(let names in obj){
+    console.log(names , ":", obj[names]);
+}
+// console.log(greet(5))
+let ans = sum(2,3);
+// console.log(ans);
+
